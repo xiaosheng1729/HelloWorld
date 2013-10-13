2eec3125f35d27fe91b5500ae08e8192ccf3e67b
Xiaosheng Mu <indefatigablexs@gmail.com>
2013-10-13 17:19:55 -0400

    View controller file cleanup

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -17,13 +17,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// Hello I'm trying out git!
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+ 
 }
 
 @end
